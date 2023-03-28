0a0a5e95cdeb0462c145f709ba5dfa61c9b133a0
your_username <stztrujillo@hotmail.com>
2023-03-28 11:02:01 +0100

    set the enviroment to deploy the contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
+const { PRIVATE_KEY, API_BSC } = process.env;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.10",
+  defaultNetwork: "hardhat",
+  networks: {
+    bsc_testnet: {
+      url: "https://endpoints.omniatech.io/v1/bsc/testnet/public",
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: API_BSC,
+  },
 };
